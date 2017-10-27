047f177a569df8c2b34b300dc7dc975e7e9ea1bc
n2omatt <n2omatt@amazingcow.com>
2017-10-27 17:24:14 -0200

    - Add the doxygen comments.

--- include/CoreFile.h
+++ include/CoreFile.h
@@ -33,9 +33,13 @@ NS_COREFILE_BEGIN
 ////////////////////////////////////////////////////////////////////////////////
 // Enums / Constants / Typedefs                                               //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Just to reduce verbosity and let clear what this type means.
 using byte_t = unsigned char;
-using tm_t   = struct tm;
 
+///@brief Just to reduce verbosity and let clear what this type means.
+using tm_t = struct tm;
+
+///@brief The mode that file will be opened.
 namespace FileMode
 {
     namespace Text {
@@ -65,45 +69,81 @@ namespace FileMode
 ////////////////////////////////////////////////////////////////////////////////
 // Append                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
-//Appends lines to a file, and then closes the file.
-//If the specified file does not exist, this method creates a file,
-//writes the specified lines to the file, and then closes the file.
+///@brief
+///  Appends lines to a file, and then closes the file.
+///  If the specified file does not exist, this method creates a file,
+///  writes the specified lines to the file, and then closes the file.
+///@param filename
+///  Filename where the lines will be written.
+///@param lines
+///  The content that will be written at file.
 void AppendAllLines(
     const std::string              &filename,
     const std::vector<std::string> &lines);
 
-//Opens a file, appends the specified string to the file,
-//and then closes the file.
-//If the file does not exist, this method creates a file, writes
-//the specified string to the file, then closes the file.
+///@brief
+///  Opens a file, appends the specified string to the file,
+///  and then closes the file.
+///  If the file does not exist, this method creates a file, writes
+///  the specified string to the file, then closes the file.
+///@param filename
+///  Filename where the content will be written.
+///@param contents
+///  The content that will be written.
 void AppendAllText(const std::string &filename, const std::string &contents);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Copy                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
-//Copies an existing file to a new file.
-//Overwriting a file of the same name is allowed.
+///@brief
+///  Copies an existing file to a new file.
+///  Overwriting a file of the same name is allowed.
+///@param src
+///  The source file.
+///@param dst
+///  The destination file.
+///@param overwrite
+///  If true destination will be overwritten if it already exists.
 void Copy(const std::string &src, const std::string &dst, bool overwrite);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Create                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
-//Creates or overwrites a file in the specified path.
+///@brief
+///  Creates or overwrites a file in the specified path.
+///@param filename
+///  The name of file that will be created.
+///@note
+///  This function will create the file in binary mode.
+///@return A std::fstream object.
 std::fstream Create(const std::string &filename);
+
+///@brief
+///  Creates or overwrites a file in the specified path.
+///@param filename
+///  The name of file that will be created.
+///@note
+///  This function will create the file in text mode.
+///@return A std::fstream object.
 std::fstream CreateText(const std::string &filename);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Delete                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Deletes the specified file.
+///@param filename
+///  The file name that will be deleted.
 void Delete(const std::string &filename);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Exists                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Check if file exists.
 bool Exist(const std::string &filename);
 
 //COWTODO: Gonna implement that???
@@ -113,36 +153,150 @@ bool Exist(const std::string &filename);
 ////////////////////////////////////////////////////////////////////////////////
 // Get * Time                                                                 //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Returns the creation date and time of the specified file or directory.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetCreationTime(const std::string &filename);
+
+///@brief
+///  Returns the creation date and time, in coordinated universal time (UTC),
+///  of the specified file or directory.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetCreationTimeUtc(const std::string &filename);
 
+
+///@brief
+///  Returns the date and time the specified file or directory
+///  was last accessed.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetLastAccessTime(const std::string &filename);
+
+///@brief
+///  Returns the date and time, in coordinated universal time (UTC),
+///  that the specified file or directory was last accessed.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetLastAccessTimeUtc(const std::string &filename);
 
+
+///@brief
+///  Returns the date and time the specified file or directory
+///  was last written to.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetLastWriteTime(const std::string &filename);
+
+///@brief
+///  Returns the date and time, in coordinated universal time (UTC),
+///  that the specified file or directory was last written to.
+///@param filename
+///  The name of file that will be queried.
+///@returns
+///  A tm_t object with the info about the file.
 tm_t GetLastWriteTimeUtc(const std::string &filename);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Move                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
-void Move(const std::string &src, const std::string &dst);
+///@brief
+///  Moves a specified file to a new location, providing the option to
+///  specify a new file name.
+///@param src
+///  The source filename.
+///@param dst
+///  The destination filename.
+///@param overwrite
+///  If true destination will be overwritten if it already exists.
+void Move(
+    const std::string &src,
+    const std::string &dst,
+    bool overwrite = true);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Open                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Opens a std::fstream on the specified path with read/write access.
+///@param filename
+///  The name of file that will be opened.
+///@param fileMode
+///  The desired file mode to open the file.
+///@returns A std::fstream object.
+///@see FileMode
 std::fstream Open(const std::string &filename, const std::string &fileMode);
+//COWTODO(n2omatt): Add an Open override with std::ios_base::openmode filemodes.
+
+
+///@brief
+///  Opens a std::fstream on the specified path with read/write access.
+///@param filename
+///  The name of file that will be opened.
+///@note
+///  The file will be open binary mode (FileMode::Binary::kRead).
+///@returns A std::fstream object.
 std::fstream OpenRead(const std::string &filename);
+
+///@brief
+///  Opens an existing text file for reading.
+///@param filename
+///  The name of file that will be opened.
+///@note
+///  The file will be open text mode (FileMode::Text::kRead).
+///@returns A std::fstream object.
 std::fstream OpenText(const std::string &filename);
+
+///@brief
+///  Opens an existing file or creates a new file for writing.
+///@param filename
+///  The name of file that will be opened.
+///@note
+///  The file will be open binary mode (FileMode::Binary::kRead).
+///@returns A std::fstream object.
 std::fstream OpenWrite(const std::string &filename);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Read                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Opens a binary file, reads the contents of the file into a byte array,
+///  and then closes the file.
+///@param filename
+///  The name of tile that will be read.
+///@returns
+///  A vector of bytes.
+///@see byte_t.
 std::vector<byte_t> ReadAllBytes(const std::string &filename);
+
+///@brief
+///  Opens a text file, reads all lines of the file, and then closes the file.
+///@param filename
+///  The name of tile that will be read.
+///@returns
+///  The file lines.
 std::vector<std::string> ReadAllLines(const std::string &filename);
+
+///@brief
+///  Opens a text file, reads all lines of the file, and then closes the file.
+///@param filename
+///  The name of tile that will be read.
+///@returns
+///  The entire file contents into a string.
 std::string ReadAllText(const std::string &filename);
 
 
@@ -153,33 +307,121 @@ std::string ReadAllText(const std::string &filename);
 ////////////////////////////////////////////////////////////////////////////////
 // Set * Time                                                                 //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Sets the date and time the file was created.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetCreationTime(const std::string &filename, time_t dateTime);
+
+///@brief
+///  Sets the date and time, in coordinated universal time (UTC),
+///  that the file was created.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetCreationTimeUtc(const std::string &filename, time_t dateTime);
 
+
+///@brief
+///  Sets the date and time the specified file was last accessed.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetLastAccessTime(const std::string &filename, time_t dateTime);
+
+///@brief
+///  Sets the date and time, in coordinated universal time (UTC),
+///  that the specified file was last accessed.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetLastAccessTimeUtc(const std::string &filename, time_t dateTime);
 
+
+///@brief
+///  Sets the date and time that the specified file was last written to.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetLastWriteTime(const std::string &filename, time_t dateTime);
+
+///@brief
+///  Sets the date and time, in coordinated universal time (UTC),
+///  that the specified file was last written to.
+///@param filename
+///  The name of target file.
+///@param dateTime
+///  The desired date time.
+///@see time_t
 void SetLastWriteTimeUtc(const std::string &filename, time_t dateTime);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Size                                                                       //
 ////////////////////////////////////////////////////////////////////////////////
+///@briefs
+///  Gets the size of file.
+///@param filename
+///  The target filename.
+///@returns
+///  The size of file in bytes.
 size_t GetSize(const std::string &filename);
-size_t GetSize(std::fstream &file_stream);
+
+///@briefs
+///  Gets the size of file.
+///@param fileStream
+///  The target file stream.
+///@returns
+///  The size of file in bytes.
+size_t GetSize(std::fstream &fileStream);
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Write                                                                      //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief
+///  Creates a new file, writes the specified byte array to the file,
+///  and then closes the file. If the target file already exists,
+///  it is overwritten.
+///@param filename
+///  The name of the file that will be written.
+///@param bytes
+///  The list of bytes that will be written.
+///@see byte_t.
 void WriteAllBytes(
     const std::string         &filename,
     const std::vector<byte_t> &bytes);
 
+///@brief
+///  Creates a new file, writes a collection of strings to the file,
+///  and then closes the file.
+///@param filename
+///  The name of the file that will be written.
+///@param lines
+///  The list of lines that will be written.
 void WriteAllLines(
     const std::string              &filename,
     const std::vector<std::string> &lines);
 
+///@brief
+///  Creates a new file, writes the specified string to the file,
+///  and then closes the file. If the target file already exists,
+///  it is overwritten.
+///@param filename
+///  The name of the file that will be written.
+///@param contents
+///  The text that will be written.
 void WriteAllText(
     const std::string &filename,
     const std::string &contents);
